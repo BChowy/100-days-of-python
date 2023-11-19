14cab8537d6bb7bf5a32b0cdca4f8106fb9eccc5
Bushra <75831792+BChowy@users.noreply.github.com>
2023-11-20 00:32:28 +0300

    Change data

--- Day 17/data.py
+++ Day 17/data.py
@@ -1,18 +1,34 @@
-question_data = [
-    {"text": "A slug's blood is green.", "answer": "True"},
-    {"text": "The loudest animal is the African Elephant.", "answer": "False"},
-    {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
-    {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
-    {
-        "text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home "
-                "to eat.",
-        "answer": "True"},
-    {"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.",
-     "answer": "False"},
-    {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
-    {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
-    {"text": "Google was originally called 'Backrub'.", "answer": "True"},
-    {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
-    {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
-    {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
-]
+question_data = [{"type": "boolean", "difficulty": "medium", "category": "Science: Computers",
+                  "question": "All program codes have to be compiled into an executable file in order to be run. This "
+                              "file can then be executed on any machine.",
+                  "correct_answer": "False", "incorrect_answers": ["True"]},
+                 {"type": "boolean", "difficulty": "medium", "category": "Science: Computers",
+                  "question": "&quot;Windows NT&quot; is a monolithic kernel.",
+                  "correct_answer": "False", "incorrect_answers": ["True"]},
+                 {"type": "boolean", "difficulty": "medium", "category": "Science: Computers",
+                  "question": "The open source program Redis is a relational database server.",
+                  "correct_answer": "False", "incorrect_answers": ["True"]},
+                 {"type": "boolean", "difficulty": "easy", "category": "Science: Computers",
+                  "question": "In most programming languages, the operator ++ is equivalent to the statement &quot;+= "
+                              "1&quot;.",
+                  "correct_answer": "True", "incorrect_answers": ["False"]},
+                 {"type": "boolean", "difficulty": "medium", "category": "Science: Computers",
+                  "question": "MacOS is based on Linux.", "correct_answer": "False",
+                  "incorrect_answers": ["True"]},
+                 {"type": "boolean", "difficulty": "easy", "category": "Science: Computers",
+                  "question": "The NVidia GTX 1080 gets its name because it can only render at a 1920x1080 screen "
+                              "resolution.",
+                  "correct_answer": "False", "incorrect_answers": ["True"]},
+                 {"type": "boolean", "difficulty": "easy", "category": "Science: Computers",
+                  "question": "Linus Torvalds created Linux and Git.", "correct_answer": "True",
+                  "incorrect_answers": ["False"]},
+                 {"type": "boolean", "difficulty": "hard", "category": "Science: Computers",
+                  "question": "The IBM PC used an Intel 8008 microprocessor clocked at 4.77 MHz and 8 kilobytes of "
+                              "memory.",
+                  "correct_answer": "False", "incorrect_answers": ["True"]},
+                 {"type": "boolean", "difficulty": "medium", "category": "Science: Computers",
+                  "question": "AMD created the first consumer 64-bit processor.",
+                  "correct_answer": "True", "incorrect_answers": ["False"]},
+                 {"type": "boolean", "difficulty": "easy", "category": "Science: Computers",
+                  "question": "The Windows ME operating system was released in the year 2000.",
+                  "correct_answer": "True", "incorrect_answers": ["False"]}]
