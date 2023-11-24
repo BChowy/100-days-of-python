41ae18e139d3076a05f65a2a7e687fb7f92b7803
Bushra <75831792+BChowy@users.noreply.github.com>
2023-11-24 19:30:00 +0300

    Add food class and collision with food detection

---
+++ Day 20 & 21/food.py
@@ -0,0 +1,20 @@
+from turtle import Turtle
+import random
+
+
+class Food(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("yellow")
+        self.penup()
+        self.speed("fastest")
+        # Make the circle have its default width and height, (20, 20) --> (10, 10)
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.refresh()
+
+    def refresh(self):
+        # avoid creating the circle on the edges of the 600x600 window
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.goto(random_x, random_y)
