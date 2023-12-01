03c62f082d7dfc04e4c412dffc583f34ee449070
Bushra <75831792+BChowy@users.noreply.github.com>
2023-12-01 22:45:19 +0300

    Create interface

--- Day 28/main.py
+++ Day 28/main.py
@@ -1,4 +1,4 @@
-
+from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -16,3 +16,31 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+timer_label = Label(text="Timer", font=(FONT_NAME, 40, "bold"))
+timer_label.config(fg=GREEN, bg=YELLOW, pady=5)
+timer_label.grid(column=1, row=0)
+
+# highlightthickness to hide canvas border
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+start_btn = Button(text="Start", font=(FONT_NAME, 10, "bold"))
+start_btn.config(pady=2, padx=7, highlightthickness=0)
+start_btn.grid(column=0, row=2)
+
+reset_btn = Button(text="Reset", font=(FONT_NAME, 10, "bold"))
+reset_btn.config(pady=2, padx=7, highlightthickness=0)
+reset_btn.grid(column=2, row=2)
+
+checkmark_label = Label(text="✔", font=20)
+checkmark_label.config(fg=GREEN, bg=YELLOW, pady=5)
+checkmark_label.grid(column=1, row=3)
+
+window.mainloop()
