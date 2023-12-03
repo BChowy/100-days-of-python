5b139a886a7a7a56c63ff0504a9410cfa1dc6934
Bushra <75831792+BChowy@users.noreply.github.com>
2023-12-03 03:38:58 +0300

    Create interface

--- Day 29/main.py
+++ Day 29/main.py
@@ -1,5 +1,42 @@
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(width=200, height=200, highlightthickness=0)
+lock_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=lock_img)
+canvas.grid(column=1, row=0)
+
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+
+website_entry = Entry(width=40)
+website_entry.grid(column=1, row=1, columnspan=2)
+
+username_label = Label(text="Email/Username:")
+username_label.grid(column=0, row=2)
+
+username_entry = Entry(width=40)
+username_entry.grid(column=1, row=2, columnspan=2)
+
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3)
+
+password_entry = Entry(width=22)
+password_entry.grid(column=1, row=3)
+
+generate_btn = Button(text="Generate Password")
+generate_btn.grid(column=2, row=3)
+
+add_btn = Button(text="Add", width=35)
+add_btn.grid(column=1, row=4, columnspan=2)
+
+window.mainloop()
