20157e4705f21a198d9814d0082f515de302f758
Bushra <75831792+BChowy@users.noreply.github.com>
2023-12-11 11:21:56 +0300

    Show the question on the interface

--- Day 34/ui.py
+++ Day 34/ui.py
@@ -1,10 +1,13 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 
 class QuizInterface:
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+
         self.window = Tk()
         self.window.title("Quizlet")
 
@@ -26,5 +29,10 @@ class QuizInterface:
         self.false_button = Button(image=false_img, highlightthickness=0)
         self.false_button.grid(column=1, row=2)
 
+        self.get_next_question()
+
         self.window.mainloop()
 
+    def get_next_question(self):
+        question = self.quiz.next_question()
+        self.canvas.itemconfig(self.q_text, text=question)
