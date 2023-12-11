b17528d237d79b18ad0b9860e2892b537b30dec1
Bushra <75831792+BChowy@users.noreply.github.com>
2023-12-11 10:49:18 +0300

    Create user interface

--- Day 34/ui.py
+++ Day 34/ui.py
@@ -1,2 +1,30 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
+
+
+class QuizInterface:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizlet")
+
+        self.window.config(pady=20, padx=20, bg=THEME_COLOR)
+
+        self.score_text = Label(text="Score: 0", bg=THEME_COLOR, fg="white")
+        self.score_text.grid(column=1, row=0)
+
+        self.canvas = Canvas(height=250, width=300, bg="white")
+        self.q_text = self.canvas.create_text(150, 125, width=250, text="Here is the text fot the question",
+                                              font=("Arial", 15, "italic"), fill=THEME_COLOR)
+        self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
+
+        true_img = PhotoImage(file="./images/true.png")
+        self.true_button = Button(image=true_img, highlightthickness=0)
+        self.true_button.grid(column=0, row=2)
+
+        false_img = PhotoImage(file="./images/false.png")
+        self.false_button = Button(image=false_img, highlightthickness=0)
+        self.false_button.grid(column=1, row=2)
+
+        self.window.mainloop()
 
