0d332a1af16198f9eef0c35682f6dbb30b60c3f3
Bushra <75831792+BChowy@users.noreply.github.com>
2023-12-11 12:00:52 +0300

    Make the interface functional
    - Make the questions change when button is pressed
    - Change the canvas background color depending on the answer
    - Tell the user when the quiz come to an end

--- Day 34/quiz_brain.py
+++ Day 34/quiz_brain.py
@@ -17,7 +17,7 @@ class QuizBrain:
         self.question_number += 1
         # remove html entities
         q_text = html.unescape(self.current_question.text)
-        return f"Q.{self.question_number}: {q_text} (True/False): "
+        return f"Q.{self.question_number}: {q_text}"
         # user_answer = input(f"Q.{self.question_number}: {q_text} (True/False): ")
         # self.check_answer(user_answer)
 
@@ -25,9 +25,9 @@ class QuizBrain:
         correct_answer = self.current_question.answer
         if user_answer.lower() == correct_answer.lower():
             self.score += 1
-            print("You got it right!")
+            return True
         else:
-            print("That's wrong.")
+            return False
 
-        print(f"Your current score is: {self.score}/{self.question_number}")
-        print("\n")
+        # print(f"Your current score is: {self.score}/{self.question_number}")
+        # print("\n")
--- Day 34/ui.py
+++ Day 34/ui.py
@@ -2,6 +2,8 @@ from tkinter import *
 from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
+RED = "#9e4747"
+GREEN = "#479e4b"
 
 
 class QuizInterface:
@@ -13,8 +15,8 @@ class QuizInterface:
 
         self.window.config(pady=20, padx=20, bg=THEME_COLOR)
 
-        self.score_text = Label(text="Score: 0", bg=THEME_COLOR, fg="white")
-        self.score_text.grid(column=1, row=0)
+        self.score_label = Label(text="Score: 0", bg=THEME_COLOR, fg="white")
+        self.score_label.grid(column=1, row=0)
 
         self.canvas = Canvas(height=250, width=300, bg="white")
         self.q_text = self.canvas.create_text(150, 125, width=250, text="Here is the text fot the question",
@@ -22,11 +24,11 @@ class QuizInterface:
         self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
 
         true_img = PhotoImage(file="./images/true.png")
-        self.true_button = Button(image=true_img, highlightthickness=0, command=self.true_btn)
+        self.true_button = Button(image=true_img, highlightthickness=0, command=self.true_btn_pressed)
         self.true_button.grid(column=0, row=2)
 
         false_img = PhotoImage(file="./images/false.png")
-        self.false_button = Button(image=false_img, highlightthickness=0, command=self.false_btn)
+        self.false_button = Button(image=false_img, highlightthickness=0, command=self.false_btn_pressed)
         self.false_button.grid(column=1, row=2)
 
         self.get_next_question()
@@ -35,10 +37,28 @@ class QuizInterface:
 
     def get_next_question(self):
         question = self.quiz.next_question()
-        self.canvas.itemconfig(self.q_text, text=question)
+        if self.quiz.still_has_questions():
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+            self.canvas.config(bg="white")
+            self.canvas.itemconfig(self.q_text, text=question)
+        else:
+            self.canvas.itemconfig(self.q_text, text="You've reached the end of questions!")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
 
-    def true_btn(self):
-        self.quiz.check_answer("True")
 
-    def false_btn(self):
-        self.quiz.check_answer("False")
+    def true_btn_pressed(self):
+        is_right = self.quiz.check_answer("True")
+        self.give_feedback(is_right)
+
+    def false_btn_pressed(self):
+        is_right = self.quiz.check_answer("False")
+        self.give_feedback(is_right)
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg=GREEN)
+        else:
+            self.canvas.config(bg=RED)
+
+        self.window.after(1000, self.get_next_question)
