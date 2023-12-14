414deea1ef056ceb5d2e5c8fb9c4e3d0efe9c49d
Bushra <75831792+BChowy@users.noreply.github.com>
2023-12-14 07:30:21 +0300

    Create a graph

--- Day 37/main.py
+++ Day 37/main.py
@@ -12,7 +12,21 @@ parameters = {
     "notMinor": "yes",
 }
 
-response = requests.post(url=pixela_endpoint, json=parameters)
+# response = requests.post(url=pixela_endpoint, json=parameters)
+# print(response.text)
+
+graph_endpoint = "https://pixe.la/v1/users/bchowy/graphs"
+graph_config = {
+    "id": "testgraph",
+    "name": "Coding Graph",
+    "unit": "commit",
+    "type": "int",
+    "color": "kuro",
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 print(response.text)
-
-
