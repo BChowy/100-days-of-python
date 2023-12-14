0f97f47218d7db224abe7cfd744861859c324f05
Bushra <75831792+BChowy@users.noreply.github.com>
2023-12-14 08:25:48 +0300

    Post a pixel

--- Day 37/main.py
+++ Day 37/main.py
@@ -1,5 +1,6 @@
 import os
 import requests
+from datetime import datetime
 
 TOKEN = os.getenv("PIXELA_TOKEN")
 USERNAME = "bchowy"
@@ -15,7 +16,7 @@ parameters = {
 # response = requests.post(url=pixela_endpoint, json=parameters)
 # print(response.text)
 
-graph_endpoint = "https://pixe.la/v1/users/bchowy/graphs"
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 graph_config = {
     "id": "testgraph",
     "name": "Coding Graph",
@@ -28,5 +29,16 @@ headers = {
     "X-USER-TOKEN": TOKEN
 }
 
-response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+today_date = datetime.today().strftime('%Y%m%d')
+
+post_pixel_endpoint = f"{graph_endpoint}/{graph_config["id"]}"
+pixel_data = {
+    "date": today_date,
+    "quantity": "5",
+}
+
+response = requests.post(url=post_pixel_endpoint, json=pixel_data, headers=headers)
 print(response.text)
