a52fab8c9b9cc0f05c5880cb12be220c653eb519
Bushra <75831792+BChowy@users.noreply.github.com>
2023-12-15 15:38:23 +0300

    Add auth token to sheety API

--- Day 38/main.py
+++ Day 38/main.py
@@ -8,7 +8,7 @@ nutritionix_APP_KEY = os.getenv("nutritionix_APP_KEY")
 exersice_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 sheety_endpoint = "https://api.sheety.co/bce0e9cffafb92f8c306cf27c93f3821/myWorkouts/workouts"
 
-headers = {
+nutritionix_headers = {
     "x-app-id": nutritionix_APP_ID,
     "x-app-key": nutritionix_APP_KEY,
 }
@@ -17,12 +17,15 @@ parameters = {
     "query": input("What exercises did you do today?"),
 }
 
-response = requests.post(url=exersice_endpoint, headers=headers, json=parameters)
+response = requests.post(url=exersice_endpoint, headers=nutritionix_headers, json=parameters)
 response.raise_for_status()
 
 today_date = datetime.now().strftime("%d/%m/%Y")
 now_time = datetime.now().strftime("%X")
 
+sheety_headers = {
+    "Authorization": "very1secret2token3@meow"
+}
 
 for exercise in response.json()["exercises"]:
     sheety_row = {
@@ -34,6 +37,6 @@ for exercise in response.json()["exercises"]:
             "calories": exercise["nf_calories"],
         }
     }
-    add_row = requests.post(url=sheety_endpoint, json=sheety_row)
+    add_row = requests.post(url=sheety_endpoint, headers=sheety_headers, json=sheety_row)
     add_row.raise_for_status()
     print(add_row.text)
