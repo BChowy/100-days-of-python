6e012e405ec7e680335a40921ca4c21881de84cb
Bushra <75831792+BChowy@users.noreply.github.com>
2023-12-26 23:36:05 +0300

    Authenticate with Spotify using Spotipy

--- Day 46/main.py
+++ Day 46/main.py
@@ -1,6 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
+CLIENT_ID = "SPOTIFY CLIENT ID"
+CLIENT_SECRET = "SPOTIFY CLIENT SECRET"
 
 url = "https://www.billboard.com/charts/hot-100/"
 
@@ -16,6 +20,15 @@ titles = soup.select(selector="div.o-chart-results-list-row-container > ul > li 
 
 songs_list = [title.get_text().strip() for title in titles]
 
-print(songs_list)
-
-
+# print(songs_list)
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri="http://localhost:8888/callback",
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        show_dialog=True,
+        cache_path="token.txt"
+    )
+)
+user_id = sp.current_user()["id"]
